e730529660fc03ea804ee26af36a4f7775a33aa5
Anas Abdelhakim <anasabdoali22@gmail.com>
2025-07-23 02:35:52 +1100

    fix vh

---
+++ src/hook/VhSections.ts
@@ -0,0 +1,20 @@
+// hooks/useVHFix.ts
+import { useEffect } from "react";
+
+export function useVHFix() {
+  useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVH(); // initial set
+    window.addEventListener("resize", setVH);
+    window.addEventListener("orientationchange", setVH);
+
+    return () => {
+      window.removeEventListener("resize", setVH);
+      window.removeEventListener("orientationchange", setVH);
+    };
+  }, []);
+}
